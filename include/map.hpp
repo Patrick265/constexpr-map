3f628b46a26a79ddd91a079cb519e8d13c997c80
Patrick de Jong <p.dejong@evs.com>
2023-04-21 15:09:57 +0200

    Initial commit

---
+++ include/map.hpp
@@ -0,0 +1,71 @@
+#pragma once
+
+#include <array>
+#include <utility>
+#include <optional>
+
+namespace custom {
+
+template<typename K, typename V>
+struct const_mpair
+{
+    K key;
+    V value;
+};
+
+template<std::size_t Size, typename Key, typename Value>
+class const_map
+{
+    using Container = std::array<const_mpair<Key, Value>, Size>;
+
+    private:
+        Container m_data;
+
+    public:
+        constexpr explicit const_map(Container newData) : m_data(newData)
+        {}
+
+        constexpr void operator=(Container newData)
+        {
+            m_data = newData;
+        }
+
+        [[nodiscard]] constexpr std::optional<Value> at(const Key& key) const
+        {
+            for(const auto& entry : m_data)
+            {
+                if(entry.key == key)
+                    return entry.value;
+            }
+            return {};
+        }
+
+        [[nodiscard]] constexpr std::optional<Value> operator[](const Key& key) const
+        {
+            return this->at(key);
+        }
+
+        constexpr bool assign(const Key& key, const Value& value)
+        {
+            for(auto& entry : m_data)
+            {
+                if(entry.key == key)
+                {
+                    entry.value = value;
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        [[nodiscard]] constexpr typename Container::const_iterator cbegin() const
+        {
+            return m_data.cbegin();
+        }
+
+        [[nodiscard]] constexpr typename Container::const_iterator cend() const
+        {
+            return m_data.cend();
+        }
+};
+}
---
+++ main.cpp
@@ -0,0 +1,65 @@
+#include <cstdint>
+#include <cstdio>
+#include <chrono>
+#include <algorithm>
+#include <random>
+#include <string_view>
+
+#include "map.hpp"
+
+template<std::size_t S, typename K, typename V>
+constexpr auto mapFiller()
+{
+    std::array<custom::const_mpair<K, V>, S> content = {};
+    for(std::size_t i = 0; i < S; i++)
+    {
+        constexpr char time[] = __TIME__;
+        constexpr int32_t seed = (time[0] - '0') * 100000 +
+                             (time[1] - '0') * 10000 + (time[3] - '0') * 1000 +
+                             (time[4] - '0') * 100 + (time[6] - '0') * 10 +
+                             time[7] - '0';
+        content.at(i) = { static_cast<K>(i), static_cast<V>(i * seed) };
+
+    }
+    return custom::const_map<S, K, V>{content};
+}
+
+void constMapValidator()
+{
+    constexpr std::size_t size = 100;
+    std::printf("%s\n", __TIME__);
+    constexpr custom::const_map<size, uint32_t, int32_t> cmap = mapFiller<size, uint32_t, int32_t>();
+
+
+    constexpr int32_t value_at_0 = cmap.at(0).value_or(-1);
+    constexpr int32_t value_at_1 = cmap.at(1).value_or(-1);
+
+    constexpr int32_t value_bracket_0 = cmap[0].value_or(-1);
+    constexpr int32_t value_bracket_1 = cmap[1].value_or(-1);
+
+    constexpr auto index = size + 1;
+    constexpr int32_t valNotFound = cmap[index].value_or(-1);
+
+    std::printf("K: .at(%i),\tV: %i\n", 0, value_at_0);
+    std::printf("K: .at(%i),\tV: %i\n", 1, value_at_1);
+    std::printf("K: [%i],\t\tV: %i\n", 0, value_bracket_0);
+    std::printf("K: [%i],\t\tV: %i\n", 1, value_bracket_1);
+    std::printf("K: [%lu],\tV: %i\n", index, valNotFound);
+
+    std::for_each(cmap.cbegin(), cmap.cend(), [](const auto kv) constexpr -> void {
+        std::printf("Key -> %i, Value -> %i\n", kv.key, kv.value);
+    });
+}
+
+int main()
+{
+    using namespace std::chrono;
+
+    const steady_clock::time_point begin = std::chrono::steady_clock::now();
+    constMapValidator();
+    const steady_clock::time_point end = std::chrono::steady_clock::now();
+    const auto ms = duration_cast<microseconds>(end - begin).count();
+
+    std::printf("%lu ms\n", ms);
+    return 0;
+}
